9b8603d49e37fc773406cd4d52d54fd3fb70b39e
azizrebhi <mohamedaziz.rebhi@ensi-uma.tn>
2026-05-11 18:55:54 +0100

    completed he auth tests with 6 tests and all passed test login non existent user , test wrong password login ,test register duplicate email , test loin and test register

--- app/tests/test_auth.py
+++ app/tests/test_auth.py
@@ -7,7 +7,6 @@ async def test_register_user(client):
     assert "id" in response.json()
     assert response.json()["email"] == "test@example.com"
 async def test_login_user(client):
-    # First, register the user
     register_data = {"email": "test@example.com", "password": "password123"}
     await client.post("/auth/register", json=register_data)
     login_data = {"username": "test@example.com", "password": "password123"}
@@ -15,3 +14,32 @@ async def test_login_user(client):
     assert response.status_code == 200
     assert "access_token" in response.json()
     assert response.json()["token_type"] == "bearer"
+async def test_register_duplicate_email(client):
+    data = {"email": "test@example.com", "password": "password123"}
+    response = await client.post("/auth/register", json=data)
+    response1 = await client.post("/auth/register", json=data)
+    assert response1.status_code==400
+    
+
+async def test_login_wrong_password(client):
+    # TODO: Register a user
+    data = {"email": "test@example.com", "password": "password123"}
+    response = await client.post("/auth/register", json=data)
+    login_data = {"username": "test@example.com", "password": "passwoord123"}
+    response1= await client.post("auth/login", data=login_data)
+    assert response1.status_code==401
+    
+
+async def test_login_nonexistent_user(client):
+    login_data = {"username": "test@example.com", "password": "password123"}
+    response1= await client.post("auth/login", data=login_data)
+    assert response1.status_code==401
+
+   
+
+async def test_get_current_user(auth_client):
+    response=await auth_client.get("auth/me")
+    # TODO: GET /auth/me with auth_client
+    assert "email" in response.json()
+    assert "id" in response.json()
+    
